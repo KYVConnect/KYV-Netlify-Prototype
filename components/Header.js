a52ca569c2e215a2e7aae0bf39cec12e2405e68e
xynrgy <70825837+xynrgys@users.noreply.github.com>
2023-07-20 16:20:18 -0700

    header update

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,32 @@
-export default function Header({ title }) {
-  return <h1 className="title">{title}</h1>
-}
+// components/Header.js
+import styles from './Header.module.css'; // Import your CSS module
+
+export default function Header(){
+  return (
+      <header className={styles.header}>
+        <nav className={styles.navbar}>
+          <div className={styles.logo}>
+            <Link href="/">JobSite</Link>
+          </div>
+          <ul className={styles.navLinks}>
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/job-listings">Job Listings</Link>
+            </li>
+            <li>
+              <Link href="/saved-jobs">Saved Jobs</Link>
+            </li>
+            <li>
+              <Link href="/applications">Applications</Link>
+            </li>
+            <li>
+              <Link href="/profile">Profile</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+  );
+};
+
