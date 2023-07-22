725e2444239e8d0ca7b7c6066738d24b02adb2dd
xynrgy <70825837+xynrgys@users.noreply.github.com>
2023-07-22 11:17:13 -0700

    improved visuals

--- components/TopNavigation.js
+++ components/TopNavigation.js
@@ -1,5 +1,14 @@
+import {FaConnectdevelop} from "react-icons/fa"
+
 export default function TopNavigation(){
     return(
-        <div className='flex border-2 border-red-500'>TOP NAVIGATION</div>
+        <div className='flex justify-between border-2 border-red-500 p-4'>
+            <div className="flex items-center gap-2">
+                <FaConnectdevelop size={30}/>
+                <div className="flex items-center gap-2">KYVConnect</div>
+            </div>
+            <div className="border-2 border-green-500">SEARCH</div>
+            <div className="border-2 border-blue-500">USER</div>
+        </div>
     );
 };
