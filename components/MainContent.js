436a17ad2c0a1b5ba47c7d847468e2b4291538d9
xynrgy <70825837+xynrgys@users.noreply.github.com>
2023-07-23 14:55:13 -0700

    Finished Home Page Wireframe

--- components/MainContent.js
+++ components/MainContent.js
@@ -22,7 +22,199 @@ export default function MainContent(){
                         </div>
                     </div>
                 </div>
-                <div className="flex border-2 border-green-500">
+                <div className="flex flex-1 border-2 border-green-500">
+                    Summary
+                </div>
+            </div>
+
+            <div className="flex flex-col col-span-1 border-2 border-blue-500">
+                <div className="flex border-2 border-blue-500">
+                    <div className="flex flex-col p-2 border-2 border-green-500">
+                        Company Image
+                    </div>
+                    <div className="flex flex-col flex-1 border-2 border-red-500">
+                        <div className="flex">
+                            Job Title
+                        </div>
+                        <div className="flex gap-5">
+                            <div className="flex">
+                                Position Length
+                            </div>
+                            <div className="flex">
+                                Salary
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex flex-1 border-2 border-green-500">
+                    Summary
+                </div>
+            </div>
+
+            <div className="flex flex-col col-span-1 border-2 border-blue-500">
+                <div className="flex border-2 border-blue-500">
+                    <div className="flex flex-col p-2 border-2 border-green-500">
+                        Company Image
+                    </div>
+                    <div className="flex flex-col flex-1 border-2 border-red-500">
+                        <div className="flex">
+                            Job Title
+                        </div>
+                        <div className="flex gap-5">
+                            <div className="flex">
+                                Position Length
+                            </div>
+                            <div className="flex">
+                                Salary
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex flex-1 border-2 border-green-500">
+                    Summary
+                </div>
+            </div>
+
+            <div className="flex flex-col col-span-1 border-2 border-blue-500">
+                <div className="flex border-2 border-blue-500">
+                    <div className="flex flex-col p-2 border-2 border-green-500">
+                        Company Image
+                    </div>
+                    <div className="flex flex-col flex-1 border-2 border-red-500">
+                        <div className="flex">
+                            Job Title
+                        </div>
+                        <div className="flex gap-5">
+                            <div className="flex">
+                                Position Length
+                            </div>
+                            <div className="flex">
+                                Salary
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex flex-1 border-2 border-green-500">
+                    Summary
+                </div>
+            </div>
+
+            <div className="flex flex-col col-span-1 border-2 border-blue-500">
+                <div className="flex border-2 border-blue-500">
+                    <div className="flex flex-col p-2 border-2 border-green-500">
+                        Company Image
+                    </div>
+                    <div className="flex flex-col flex-1 border-2 border-red-500">
+                        <div className="flex">
+                            Job Title
+                        </div>
+                        <div className="flex gap-5">
+                            <div className="flex">
+                                Position Length
+                            </div>
+                            <div className="flex">
+                                Salary
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex flex-1 border-2 border-green-500">
+                    Summary
+                </div>
+            </div>
+
+            <div className="flex flex-col col-span-1 border-2 border-blue-500">
+                <div className="flex border-2 border-blue-500">
+                    <div className="flex flex-col p-2 border-2 border-green-500">
+                        Company Image
+                    </div>
+                    <div className="flex flex-col flex-1 border-2 border-red-500">
+                        <div className="flex">
+                            Job Title
+                        </div>
+                        <div className="flex gap-5">
+                            <div className="flex">
+                                Position Length
+                            </div>
+                            <div className="flex">
+                                Salary
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex flex-1 border-2 border-green-500">
+                    Summary
+                </div>
+            </div>
+
+            <div className="flex flex-col col-span-1 border-2 border-blue-500">
+                <div className="flex border-2 border-blue-500">
+                    <div className="flex flex-col p-2 border-2 border-green-500">
+                        Company Image
+                    </div>
+                    <div className="flex flex-col flex-1 border-2 border-red-500">
+                        <div className="flex">
+                            Job Title
+                        </div>
+                        <div className="flex gap-5">
+                            <div className="flex">
+                                Position Length
+                            </div>
+                            <div className="flex">
+                                Salary
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex flex-1 border-2 border-green-500">
+                    Summary
+                </div>
+            </div>
+
+            <div className="flex flex-col col-span-1 border-2 border-blue-500">
+                <div className="flex border-2 border-blue-500">
+                    <div className="flex flex-col p-2 border-2 border-green-500">
+                        Company Image
+                    </div>
+                    <div className="flex flex-col flex-1 border-2 border-red-500">
+                        <div className="flex">
+                            Job Title
+                        </div>
+                        <div className="flex gap-5">
+                            <div className="flex">
+                                Position Length
+                            </div>
+                            <div className="flex">
+                                Salary
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex flex-1 border-2 border-green-500">
+                    Summary
+                </div>
+            </div>
+
+            <div className="flex flex-col col-span-1 border-2 border-blue-500">
+                <div className="flex border-2 border-blue-500">
+                    <div className="flex flex-col p-2 border-2 border-green-500">
+                        Company Image
+                    </div>
+                    <div className="flex flex-col flex-1 border-2 border-red-500">
+                        <div className="flex">
+                            Job Title
+                        </div>
+                        <div className="flex gap-5">
+                            <div className="flex">
+                                Position Length
+                            </div>
+                            <div className="flex">
+                                Salary
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex flex-1 border-2 border-green-500">
                     Summary
                 </div>
             </div>
