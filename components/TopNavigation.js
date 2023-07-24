dd16d82d5498fc64e95fff7385144990b4a8e3f8
xynrgy <70825837+xynrgys@users.noreply.github.com>
2023-07-23 19:26:13 -0700

    updated top navbar

--- components/TopNavigation.js
+++ components/TopNavigation.js
@@ -1,4 +1,9 @@
 import {FaConnectdevelop} from "react-icons/fa"
+import {AiOutlineSearch} from "react-icons/ai"
+import {CgProfile} from "react-icons/cg"
+import {FiSettings} from "react-icons/fi"
+
+import Link from 'next/Link'
 
 export default function TopNavigation(){
     return(
@@ -7,8 +12,23 @@ export default function TopNavigation(){
                 <FaConnectdevelop size={30}/>
                 <div className="flex items-center gap-2">KYVConnect</div>
             </div>
-            <div className="border-2 border-green-500">SEARCH</div>
-            <div className="border-2 border-blue-500">USER</div>
+            <div className="flex rounded-xl border-2 gap-4 p-2 border-zinc-500">
+                <input type="search" 
+                    placeholder="Find Jobs" 
+                    className="flex w-full bg-transparent outline-none items-center">
+                </input>
+                <div className="flex items-center">
+                    <AiOutlineSearch size={20}/>
+                </div>
+            </div>
+            <div className="flex gap-4 p-2">
+                <div>
+                    <CgProfile size={25}/>
+                </div>
+                <div>
+                    <FiSettings size={24}/>
+                </div>
+            </div>
         </div>
     );
 };
