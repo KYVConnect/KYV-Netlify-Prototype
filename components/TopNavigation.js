6538d5304ab2e1da08e160731652b0bc52522630
xynrgy <70825837+xynrgys@users.noreply.github.com>
2023-07-23 19:29:55 -0700

    made searchbar dynamic

--- components/TopNavigation.js
+++ components/TopNavigation.js
@@ -7,12 +7,12 @@ import Link from 'next/Link'
 
 export default function TopNavigation(){
     return(
-        <div className='flex justify-between border-2 border-red-500 p-4'>
+        <div className='flex justify-between gap-20 border-2 border-red-500 p-4'>
             <div className="flex items-center gap-2">
                 <FaConnectdevelop size={30}/>
                 <div className="flex items-center gap-2">KYVConnect</div>
             </div>
-            <div className="flex rounded-xl border-2 gap-4 p-2 border-zinc-500">
+            <div className="flex rounded-xl flex-grow border-2 gap-4 p-2 border-zinc-500">
                 <input type="search" 
                     placeholder="Find Jobs" 
                     className="flex w-full bg-transparent outline-none items-center">
